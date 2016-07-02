4c6bc7d0eb9550e61e1f48be67211b7ba33bb9a5
Marat K <janmarat@gmail.com>
2016-07-01 21:36:29 -0700

    More test coverage

--- HomeworkA/API.Test/StopsServiceTests.cs
+++ HomeworkA/API.Test/StopsServiceTests.cs
@@ -43,5 +43,89 @@ namespace API.Test
             Assert.AreEqual(new TimeSpan(15, 4, 0), arrivalTimes[2].ArrivalTimes.ToList()[0]);
             Assert.AreEqual(new TimeSpan(15, 19, 0), arrivalTimes[2].ArrivalTimes.ToList()[1]);
         }
+
+        [TestMethod]
+        public void StopsService_GetClosestArrivalTimes_AtCurrentArrivalTime()
+        {
+            // Arrange
+            var timeSpan = new TimeSpan(0, 0, 0);
+            var stopNumber = 1;
+
+            // Act
+            var arrivalTimes = stopsService.GetClosestArrivalTimes(stopNumber, timeSpan).ToList();
+
+            // Assert
+            Assert.IsNotNull(arrivalTimes);
+            Assert.AreEqual(3, arrivalTimes.Count);
+
+            Assert.AreEqual("Route 1", arrivalTimes[0].RouteName);
+            Assert.AreEqual("Route 2", arrivalTimes[1].RouteName);
+            Assert.AreEqual("Route 3", arrivalTimes[2].RouteName);
+
+            Assert.AreEqual(new TimeSpan(0, 15, 0), arrivalTimes[0].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 30, 0), arrivalTimes[0].ArrivalTimes.ToList()[1]);
+
+            Assert.AreEqual(new TimeSpan(0, 2, 0), arrivalTimes[1].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 17, 0), arrivalTimes[1].ArrivalTimes.ToList()[1]);
+
+            Assert.AreEqual(new TimeSpan(0, 4, 0), arrivalTimes[2].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 19, 0), arrivalTimes[2].ArrivalTimes.ToList()[1]);
+        }
+
+        [TestMethod]
+        public void StopsService_GetClosestArrivalTimes_BeforeFirstStopTime()
+        {
+            // Arrange
+            var timeSpan = new TimeSpan(0, 0, 0);
+            var stopNumber = 2;
+
+            // Act
+            var arrivalTimes = stopsService.GetClosestArrivalTimes(stopNumber, timeSpan).ToList();
+
+            // Assert
+            Assert.IsNotNull(arrivalTimes);
+            Assert.AreEqual(3, arrivalTimes.Count);
+
+            Assert.AreEqual("Route 1", arrivalTimes[0].RouteName);
+            Assert.AreEqual("Route 2", arrivalTimes[1].RouteName);
+            Assert.AreEqual("Route 3", arrivalTimes[2].RouteName);
+
+            Assert.AreEqual(new TimeSpan(0, 2, 0), arrivalTimes[0].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 17, 0), arrivalTimes[0].ArrivalTimes.ToList()[1]);
+
+            Assert.AreEqual(new TimeSpan(0, 4, 0), arrivalTimes[1].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 19, 0), arrivalTimes[1].ArrivalTimes.ToList()[1]);
+
+            Assert.AreEqual(new TimeSpan(0, 6, 0), arrivalTimes[2].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 21, 0), arrivalTimes[2].ArrivalTimes.ToList()[1]);
+        }
+
+        [TestMethod]
+        public void StopsService_GetClosestArrivalTimes_WhenLastArrivalTimeIsAfterMidnight()
+        {
+            // Arrange
+            var timeSpan = new TimeSpan(0, 0, 0);
+            var stopNumber = 10;
+
+            // Act
+            var arrivalTimes = stopsService.GetClosestArrivalTimes(stopNumber, timeSpan).ToList();
+
+            // Assert
+            Assert.IsNotNull(arrivalTimes);
+            Assert.AreEqual(3, arrivalTimes.Count);
+
+            Assert.AreEqual("Route 1", arrivalTimes[0].RouteName);
+            Assert.AreEqual("Route 2", arrivalTimes[1].RouteName);
+            Assert.AreEqual("Route 3", arrivalTimes[2].RouteName);
+
+            Assert.AreEqual(new TimeSpan(0, 3, 0), arrivalTimes[0].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 18, 0), arrivalTimes[0].ArrivalTimes.ToList()[1]);
+
+            Assert.AreEqual(new TimeSpan(0, 5, 0), arrivalTimes[1].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 20, 0), arrivalTimes[1].ArrivalTimes.ToList()[1]);
+
+            Assert.AreEqual(new TimeSpan(0, 7, 0), arrivalTimes[2].ArrivalTimes.ToList()[0]);
+            Assert.AreEqual(new TimeSpan(0, 22, 0), arrivalTimes[2].ArrivalTimes.ToList()[1]);
+        }
     }
 }
